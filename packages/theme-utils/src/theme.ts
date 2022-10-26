19c4df82fe70a74294c5c7b114470d1581da5abd
Shyrro <zsahmane@gmail.com>
2022-10-26 23:37:02 +0200

    feat(emotion): add emotioncss
    Signed-off-by: Shyrro <zsahmane@gmail.com>

---
+++ packages/theme-utils/src/theme.ts
@@ -0,0 +1,9 @@
+export default {
+  components: {
+    Tag: {
+      baseStyles: {
+        background: 'red',
+      },
+    },
+  },
+};
--- playground/src/histoire.setup.ts
+++
@@ -1,3 +0,0 @@
-import initUnocssRuntime from '@unocss/runtime'
-
-initUnocssRuntime({})
